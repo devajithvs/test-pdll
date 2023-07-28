a8afe4d1e1714fd5781541019951f3d3737a92da
Devajith Valaparambil Sreeramaswamy <devajithvs@gmail.com>
2023-07-28 13:10:27 +0100

    Testing PDLL patterns for matching

--- testPDLL.cpp
+++ testPDLL.cpp
@@ -1,17 +1,21 @@
+#include "ByteCode.h"
 #include "mlir/Dialect/PDL/IR/PDL.h"
 #include "mlir/Dialect/PDLInterp/IR/PDLInterp.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/PatternMatch.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Pass/PassManager.h"
+#include "mlir/Rewrite/PatternApplicator.h"
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Tools/ParseUtilities.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-#include "testPDLL.h.inc"
 #include "llvm/Support/SourceMgr.h"
 #include <iostream>
+
+#include "testPDLL.h.inc"
 
 using namespace mlir;
 using namespace llvm;
@@ -19,6 +23,14 @@ using namespace llvm;
 //===----------------------------------------------------------------------===//
 // Query Parser
 //===----------------------------------------------------------------------===//
+
+/// Define a custom PatternRewriter for use by the driver.
+class MyPatternRewriter : public PatternRewriter {
+public:
+  MyPatternRewriter(MLIRContext *ctx) : PatternRewriter(ctx) {}
+
+  /// Override the necessary PatternRewriter hooks here.
+};
 
 mlir::LogicalResult testPDLLMain(int argc, char **argv, MLIRContext &context) {
   // Set up the input file.
@@ -43,14 +55,41 @@ mlir::LogicalResult testPDLLMain(int argc, char **argv, MLIRContext &context) {
 
   Operation *rootOp = opRef.get();
 
-  rootOp->dump();
+  // rootOp->dump();
 
   RewritePatternSet patternList(&context);
   populateGeneratedPDLLPatterns(patternList);
-  FrozenRewritePatternSet patterns = std::move(patternList);
-  (void)applyPatternsAndFoldGreedily(rootOp, patterns);
+  FrozenRewritePatternSet frozenPatternList = std::move(patternList);
 
-  rootOp->dump();
+  MyPatternRewriter rewriter(&context);
+
+  SourceMgrDiagnosticHandler sourceMgrHandler(*sourceMgr, &context);
+
+  const mlir::detail::PDLByteCode *bytecode =
+      frozenPatternList.getPDLByteCode();
+  unsigned prevSize = 0;
+  unsigned matchCount = 0;
+  if (bytecode) {
+    auto mutableByteCodeState =
+        std::make_unique<mlir::detail::PDLByteCodeMutableState>();
+    SmallVector<mlir::detail::PDLByteCode::MatchResult, 4> pdlMatches;
+    bytecode->initializeMutableState(*mutableByteCodeState);
+    rootOp->walk([&](Operation *op) {
+      bytecode->match(op, rewriter, pdlMatches, *mutableByteCodeState);
+      matchCount = pdlMatches.size();
+      if (matchCount != prevSize) {
+        llvm::errs() << "Match #" << matchCount << ":\n\n";
+        op->emitRemark("\"root\" binds here");
+      }
+      prevSize = matchCount;
+    });
+    llvm::errs() << matchCount
+                 << (matchCount == 1 ? " match.\n\n" : " matches.\n\n");
+  }
+
+  // (void)applyPatternsAndFoldGreedily(rootOp, patterns);
+
+  // rootOp->dump();
 
   return success();
 }
